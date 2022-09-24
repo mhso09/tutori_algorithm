bdd7a4b8363322b5eddc8c31d0a929f50c6ca754
mhso09 <mhso0@naver.com>
2022-09-24 16:30:28 +0900

    9/24 search

--- chap3/chap3_search_berbose.py
+++ chap3/chap3_search_berbose.py
@@ -3,20 +3,41 @@ from typing import Any, Sequence
 def seq_search(a: Sequence, key: Any) -> int:
     # 시퀸스 a에서 key와 값이 같은 원소를 이진 검색
     pl = 0 # 검색범위 맨 앞 원소의 인덱스
-    pr = len(a)-1 # 검색범위 맨 뒤 원소의 인덱스
+    pr = len(a) - 1 # 검색범위 맨 뒤 원소의 인덱스
+
+    print('   |', end='')
+    for i in range(len(a)):
+        print(f'{i : 4}', end='')
+    print()
+    print('---+' + (4 * len(a) +2) * '-')
 
     while True:
         pc = (pl + pr) // 2 # 중앙 원소의 인덱스
-        if a[pc] == key:
-            return pc # 검색 성공
-        elif a[pc] < key:
-            pl = pc + 1 # 검색 범위를 뒤쪽 절반으로 좁힘
+
+        print('   |', end='')
+        if pl != pc:
+            print((pl * 4 + 1) * ' ' + '<-' + ((pc -pl) * 4) * ' ' + '+',end='')
         else:
-            pr = pc - 1 # 검색 범위를 앞쪽 절반으로 좁힘
+            print((pc * 4 + 1) * ' ' + '<+', end='')
+        if pc != pr:
+            print(((pr - pc) * 4 -2) * ' ' + '->')
+        else:
+            print('->')
+        print(f'{pc:3}|',end='')
+        for i in range(len(a)):
+            print(f'{a[i]:4}',end='')
+        print('\n   |')
+
+
+        if a[pc] == key:
+            return pc  # 검색 성공
+        elif a[pc] < key:
+            pl = pc + 1  # 검색 범위를 뒤쪽 절반으로 좁힘
+        else:
+            pr = pc - 1  # 검색 범위를 앞쪽 절반으로 좁힘
         if pl > pr:
             break
-    return -1 # 검색 실패
-
+    return -1  # 검색 실패
 
 if __name__ == '__main__':
     num = int(input('원소 수를 입력하세요 : '))  # num값을 입력받음
@@ -28,7 +49,7 @@ if __name__ == '__main__':
 
     for i in range(1, num):
         while True:
-            x[i] = int(input(f'x[{i}]: '))
+            x[i] = int(input(f'x[{i}]:'))
             if x[i] >= x[i - 1]: # 바로 직전에 입력한 원소값보다 큰 값을 입력
                 break
 
